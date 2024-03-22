cb442bab628d12bdab56f6826f3a6aae966f8a43
clintcs <114178960+clintcs@users.noreply.github.com>
2024-03-22 14:14:08 -0500

    Add more linting rules

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This repository contains all packages for the Glide Design System.
 
 ## Development
 
+### Contributing
+
+Follow our [Contribution Guidelines](./CONTRIBUTING.md).
+
 ### Getting started
 
 First you'll need to install the dependencies for the repository. We use [PNPM](https://pnpm.io). Run the following command from the root of the repository:
@@ -24,7 +28,3 @@ pnpm start
 
 After Storybook's build completes, your browser should navigate to the Storybook instance.
 If this doesn't happen automatically, a URL will be shown in your terminal for you to browse to.
-
-### Contributing
-
-Follow our [Contribution Guidelines](./CONTRIBUTING.md).
--- packages/components/eslint.config.js
+++ packages/components/eslint.config.js
@@ -3,12 +3,15 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import globals from 'globals';
 import js from '@eslint/js';
+import sortClassMembers from 'eslint-plugin-sort-class-members';
+import stylistic from '@stylistic/eslint-plugin';
 
 const compat = new FlatCompat();
 
 export default [
   js.configs.recommended,
   eslintPluginUnicorn.configs['flat/all'],
+  sortClassMembers.configs['flat/recommended'],
   ...compat.extends('plugin:@typescript-eslint/recommended'),
   ...compat.extends('plugin:lit/recommended'),
   ...compat.extends('plugin:lit-a11y/recommended'),
@@ -18,7 +21,25 @@ export default [
     ignores: ['dist'],
   },
   {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
+      '@stylistic/lines-between-class-members': [
+        'error',
+        'always',
+        { exceptAfterSingleLine: false },
+      ],
+
+      // We work with the DOM enough that this rule also became tiresome.
+      'unicorn/no-null': 'off',
+
+      // We want to steer people toward using real member privacy.
+      'no-underscore-dangle': [
+        'error',
+        { enforceInClassFields: true, enforceInMethodNames: true },
+      ],
+
       'prefer-const': 'error',
       'sort-imports-es6-autofix/sort-imports-es6': [
         2,
@@ -28,9 +49,11 @@ export default [
           memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
         },
       ],
+
       // We use Lit's `classMap` often enough that disabling this rule ad hoc
       // became tiresome.
       'unicorn/no-keyword-prefix': 'off',
+
       'no-restricted-imports': [
         'error',
         {
@@ -53,19 +76,90 @@ export default [
             "Please use separate files rather than the 'describe' block.",
         },
       ],
+      'sort-class-members/sort-class-members': [
+        2,
+        {
+          order: [
+            '[public-static-properties]',
+            '[public-@property]',
+            '[public-@state]',
+            '[public-properties]',
+            '[public-accessors]',
+            '[public-arrow-function-expressions]',
+            '[public-methods]',
+            '[public-static-methods]',
+
+            'constructor',
+
+            '[private-static-properties]',
+            '[private-@state]',
+            '[private-properties]',
+            '[private-accessors]',
+            '[private-arrow-function-expressions]',
+            '[private-methods]',
+          ],
+          accessorPairPositioning: 'getThenSet',
+          groups: {
+            'public-static-properties': [
+              { type: 'property', sort: 'alphabetical', static: true },
+            ],
+            'public-@property': [
+              {
+                groupByDecorator: '/property/',
+                sort: 'alphabetical',
+              },
+            ],
+            'public-@state': [
+              {
+                groupByDecorator: '/state/',
+                sort: 'alphabetical',
+              },
+            ],
+            'public-properties': [{ type: 'property', sort: 'alphabetical' }],
+            'public-accessors': [{ kind: 'accessor' }],
+            'public-methods': [{ type: 'method', sort: 'alphabetical' }],
+            'public-arrow-function-expressions': [
+              {
+                type: 'property',
+                propertyType: 'ArrowFunctionExpression',
+                sort: 'alphabetical',
+              },
+            ],
+            'public-static-methods': [
+              { type: 'method', sort: 'alphabetical', static: true },
+            ],
+            'private-@state': [
+              {
+                type: 'property',
+                groupByDecorator: '/state/',
+                accessibility: 'private',
+                sort: 'alphabetical',
+              },
+            ],
+            'private-properties': [
+              { type: 'property', private: true, sort: 'alphabetical' },
+            ],
+            'private-accessors': [{ kind: 'accessor', private: true }],
+            'private-arrow-function-expressions': [
+              {
+                type: 'property',
+                private: true,
+                propertyType: 'ArrowFunctionExpression',
+                sort: 'alphabetical',
+              },
+            ],
+            'private-methods': [
+              { type: 'method', private: true, sort: 'alphabetical' },
+            ],
+          },
+        },
+      ],
     },
   },
   {
     files: ['*.js', '*.ts', '**/*.test.ts'],
     languageOptions: {
       globals: globals.node,
-    },
-  },
-  {
-    files: ['**/*.test.ts', '**/*.test.*.ts'],
-    rules: {
-      // `null` is commonly used as a value in DOM APIs, which tests often test.
-      'unicorn/no-null': 'off',
     },
   },
 ];
