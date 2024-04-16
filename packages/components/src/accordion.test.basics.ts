d9218570a8d3eec817d523819fd15382ea534c6c
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-04-16 14:46:16 -0400

    Add accordion component (#35)
    * Copy and save files from previous repo
    
    * Remove cs- prefix from toggle event
    
    * Change way of opening shadow root
    
    * Replace icons
    
    * Update story to match new patterns
    
    * Update test patterns
    
    * Update to CSS logical properties
    
    * Run stylelint formatting
    
    * Remove BEM classes per stylelint in favor of kebab-case
    
    * Add tests for 100% coverage
    
    * Remove unhelpful comments
    
    * troubleshoot CI
    
    * Revert "troubleshoot CI"
    
    This reverts commit 99e42866c8342e73d7ca3f36b7cfb97eea08dafb.
    
    * Increase test coverage
    
    * Revert "Increase test coverage"
    
    This reverts commit 00d1bc9eb2908a97c571a7ef5bf6a0e549a688fa.
    
    * Force animation finishes
    
    * Rename internal event handlers
    
    * Call 'finish' on the animated div as well
    
    * Added a changeset
    
    * Rename import to CsAccordion to match Menu naming
    
    * Revert "Rename import to CsAccordion to match Menu naming"
    
    This reverts commit 969da7579408e0a4e14d9938c6c721b93594994f.
    
    * Rename import to CsAccordion to match Menu naming
    
    * Dont rely on toggle, but click handlers for events

---
+++ packages/components/src/accordion.test.basics.ts
@@ -0,0 +1,147 @@
+import './accordion.js';
+import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
+import CsAccordion from './accordion.js';
+
+CsAccordion.shadowRootOptions.mode = 'open';
+
+it('registers', async () => {
+  expect(window.customElements.get('cs-accordion')).to.equal(CsAccordion);
+});
+
+it('is accessible', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"></cs-accordion>`,
+  );
+
+  await expect(component).to.be.accessible();
+});
+
+it('is closed by default', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"></cs-accordion>`,
+  );
+
+  const accordion =
+    component.shadowRoot?.querySelector<HTMLDetailsElement>('details');
+
+  expect(accordion).to.be.ok;
+  expect(accordion?.hasAttribute('open')).to.be.false;
+});
+
+it('defaults to "open" when provided with the attribute', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label" open></cs-accordion>`,
+  );
+
+  const accordion =
+    component.shadowRoot?.querySelector<HTMLDetailsElement>('details');
+
+  expect(accordion).to.be.ok;
+  expect(accordion?.hasAttribute('open')).to.be.true;
+});
+
+it('renders the provided "label"', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="Accordion Title"></cs-accordion>`,
+  );
+
+  const label = component.shadowRoot?.querySelector<HTMLSpanElement>(
+    '[data-test="label"]',
+  );
+
+  expect(label).to.be.ok;
+  expect(label?.textContent?.trim()).to.equal('Accordion Title');
+});
+
+it('renders the provided default slotted content', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"
+      ><p data-body>Inner content</p></cs-accordion
+    >`,
+  );
+
+  const body = component.querySelector<HTMLParagraphElement>('[data-body]');
+
+  expect(body).to.be.ok;
+});
+
+it('renders with a prefix slot and applies the appropriate classes', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"
+      ><span slot="prefix" data-prefix>prefix</span></cs-accordion
+    >`,
+  );
+
+  expect(document.querySelector('[data-prefix]')).to.be.ok;
+
+  expect([
+    ...component.shadowRoot!.querySelector('[data-test="label"]')!.classList,
+  ]).to.deep.equal(['heading-box', 'heading-box-with-prefix']);
+
+  expect([
+    ...component.shadowRoot!.querySelector('[role="region"]')!.classList,
+  ]).to.deep.equal(['content', 'content-with-prefix']);
+});
+
+it('does not apply prefix classes when no prefix slot is provided', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"></cs-accordion>`,
+  );
+
+  expect([
+    ...component.shadowRoot!.querySelector('[data-test="label"]')!.classList,
+  ]).to.deep.equal(['heading-box']);
+  expect([
+    ...component.shadowRoot!.querySelector('[role="region"]')!.classList,
+  ]).to.deep.equal(['content']);
+});
+
+it('renders with a suffix slot and applies the appropriate class', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"
+      ><span slot="suffix" data-suffix>suffix</span></cs-accordion
+    >`,
+  );
+
+  expect(component.querySelector('[data-suffix]')).to.be.ok;
+
+  expect([
+    ...component.shadowRoot!.querySelector('[data-test="suffix"]')!.classList,
+  ]).to.deep.equal(['suffix-slot-box', 'suffix-slot-box-with-content']);
+});
+
+it('does not apply the suffix class when no suffix slot is provided', async () => {
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"></cs-accordion>`,
+  );
+
+  expect([
+    ...component.shadowRoot!.querySelector('[data-test="suffix"]')!.classList,
+  ]).to.deep.equal(['suffix-slot-box']);
+});
+
+it('renders without prefix and suffix classes after both are removed', async () => {
+  const component = await fixture<CsAccordion>(html`
+    <cs-accordion label="label">
+      <span slot="prefix">prefix</span>
+      <span slot="suffix">suffix</span>
+    </cs-accordion>
+  `);
+
+  component.querySelector('[slot="prefix"]')?.remove();
+  component.querySelector('[slot="suffix"]')?.remove();
+  await elementUpdated(component);
+
+  // prefix
+  expect([
+    ...component.shadowRoot!.querySelector('[data-test="label"]')!.classList,
+  ]).to.deep.equal(['heading-box']);
+  expect([
+    ...component.shadowRoot!.querySelector('[role="region"]')!.classList,
+  ]).to.deep.equal(['content']);
+
+  // suffix
+  expect([
+    ...component.shadowRoot!.querySelector('[data-test="suffix"]')!.classList,
+  ]).to.deep.equal(['suffix-slot-box']);
+});
---
+++ packages/components/src/accordion.test.events.ts
@@ -0,0 +1,65 @@
+import './accordion.js';
+import {
+  elementUpdated,
+  expect,
+  fixture,
+  html,
+  oneEvent,
+} from '@open-wc/testing';
+import CsAccordion from './accordion.js';
+
+CsAccordion.shadowRootOptions.mode = 'open';
+
+it('dispatches a "toggle" event when the Accordion opens', async () => {
+  let hasToggleBeenCalled = false;
+
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label"></cs-accordion>`,
+  );
+  component.addEventListener('toggle', () => (hasToggleBeenCalled = true));
+
+  const summary = component.shadowRoot?.querySelector<HTMLElement>(
+    '[data-test="summary"]',
+  );
+  expect(summary).to.be.ok;
+
+  summary?.click();
+
+  await oneEvent(component, 'toggle');
+
+  expect(hasToggleBeenCalled).to.be.true;
+});
+
+it('dispatches a "toggle" event when the Accordion closes', async () => {
+  let hasToggleBeenCalled = false;
+
+  const component = await fixture<CsAccordion>(
+    html`<cs-accordion label="label" open></cs-accordion>`,
+  );
+  component.addEventListener('toggle', () => (hasToggleBeenCalled = true));
+
+  const summary = component.shadowRoot?.querySelector<HTMLElement>(
+    '[data-test="summary"]',
+  );
+  expect(summary).to.be.ok;
+
+  summary?.click();
+
+  // Force the animations to complete with javascript
+  // and by triggering a `finish` event ourselves.
+  component.shadowRoot
+    ?.querySelector('[data-test="content"]')
+    ?.getAnimations()
+    ?.at(0)
+    ?.finish();
+
+  component.shadowRoot
+    ?.querySelector('[data-test="content"]')
+    ?.dispatchEvent(new AnimationEvent('finish'));
+
+  await elementUpdated(component);
+
+  await oneEvent(component, 'toggle');
+
+  expect(hasToggleBeenCalled).to.be.true;
+});
