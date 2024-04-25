e545b699d0839176be7e3b517351f79f85b97eac
Dan Wenzel <11724146+danwenzel@users.noreply.github.com>
2024-04-25 15:34:31 -0500

    Reduce font size in tree item label container

--- packages/components/src/tree-item.styles.ts
+++ packages/components/src/tree-item.styles.ts
@@ -43,6 +43,7 @@ export default [
       border-radius: 0.625rem;
       color: var(--cs-text-body-1);
       display: flex;
+      font-size: var(--cs-body-sm-font-size);
       padding-block: var(--cs-spacing-xxs);
       padding-inline: var(--cs-spacing-xs);
 
