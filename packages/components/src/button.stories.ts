68358195c97f6fba8f2d46931f729e069866d4c0
clintcs <114178960+clintcs@users.noreply.github.com>
2024-05-08 15:22:34 -0500

    Add Dropdown "quiet" variant (#96)

--- packages/components/src/button.stories.ts
+++ packages/components/src/button.stories.ts
@@ -9,7 +9,7 @@ const meta: Meta = {
   parameters: {
     docs: {
       description: {
-        component: 'A button element with optional slots for icons.',
+        component: 'A button with optional slots for icons.',
       },
     },
   },
