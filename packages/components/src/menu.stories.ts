648831bdbddce5a5b89ed792e01bf191535e4bad
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2024-05-10 12:08:26 -0400

    Test for cs-menu wrap issue

--- packages/components/src/menu.stories.ts
+++ packages/components/src/menu.stories.ts
@@ -1,4 +1,5 @@
 import './button.js';
+import './icon-button.js';
 import './icons/storybook.js';
 import './menu.button.js';
 import './menu.js';
@@ -64,7 +65,11 @@ const meta: Meta = {
     /* eslint-disable unicorn/explicit-length-check */
     return html`<div style="height: 8rem;">
       <cs-menu size=${arguments_.size || nothing} ?open=${arguments_.open}>
-        <cs-menu-link label="One" url="/one"> </cs-menu-link>
+        <cs-menu-link
+          label="One One OneOneOne OneOne OneOne OneOneOne OneOne OneOneOneOneOne OneOne OneOneOneOneOne OneOne OneOneOneOneOne OneOne OneOne"
+          url="/one"
+        >
+        </cs-menu-link>
         <cs-menu-link label="Two" url="/two"> </cs-menu-link>
         <!--
           If an option does not have an associated url,
@@ -106,5 +111,80 @@ export const MenuWithIcon: StoryObj = {
         <cs-button slot="target" variant="secondary"> Target </cs-button>
       </cs-menu>
     </div>`;
+  },
+};
+
+export const TonyTest: StoryObj = {
+  name: 'Tony',
+  render() {
+    return html`<cs-menu size="large">
+        <cs-menu-button label="Rename">
+          <cs-example-icon slot="icon" name="pencil"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Delete">
+          <cs-example-icon slot="icon" name="calendar"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Move">
+          <cs-example-icon slot="icon" name="move"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Duplicate">
+          <cs-example-icon slot="icon" name="clipboard"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Export">
+          <cs-example-icon slot="icon" name="share"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Add to Favorites">
+          <cs-example-icon slot="icon" name="share"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Settings">
+          <cs-example-icon slot="icon" name="settings"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Performance">
+          <cs-example-icon slot="icon" name="drag-dots"></cs-example-icon>
+        </cs-menu-button>
+
+        <cs-icon-button
+          slot="target"
+          label="For screenreaders"
+          variant="tertiary"
+        >
+          <cs-example-icon name="caret-down"></cs-example-icon>
+        </cs-icon-button>
+      </cs-menu>
+
+      <cs-menu size="large" style="position: relative">
+        <cs-menu-button label="Rename">
+          <cs-example-icon slot="icon" name="pencil"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Delete">
+          <cs-example-icon slot="icon" name="calendar"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Move">
+          <cs-example-icon slot="icon" name="move"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Duplicate">
+          <cs-example-icon slot="icon" name="clipboard"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Export">
+          <cs-example-icon slot="icon" name="share"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Add to Favorites">
+          <cs-example-icon slot="icon" name="share"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Settings">
+          <cs-example-icon slot="icon" name="settings"></cs-example-icon>
+        </cs-menu-button>
+        <cs-menu-button label="Performance">
+          <cs-example-icon slot="icon" name="drag-dots"></cs-example-icon>
+        </cs-menu-button>
+
+        <cs-icon-button
+          slot="target"
+          label="For screenreaders"
+          variant="tertiary"
+        >
+          <cs-example-icon name="caret-down"></cs-example-icon>
+        </cs-icon-button>
+      </cs-menu>`;
   },
 };
