6781bba6ba25e5205ecaeeff51abfcc6ac9bd97e
Dan Wenzel <11724146+danwenzel@users.noreply.github.com>
2024-06-03 14:48:01 -0500

    Add no-nested-template-literals rule

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,4 +1,5 @@
 import { consistentReferenceElementDeclarations } from './rules/consistent-reference-element-declarations.js';
+import { noNestedTemplateLiterals } from './rules/no-nested-template-literals.js';
 import { noPrefixedEventName } from './rules/no-cs-prefixed-event-name.js';
 import { preferClosedShadowRoot } from './rules/prefer-closed-shadow-root.js';
 import { prefixedClassDeclaration } from './rules/prefixed-lit-element-class-declaration.js';
@@ -6,6 +7,7 @@ import { prefixedClassDeclaration } from './rules/prefixed-lit-element-class-dec
 const rules = {
   'consistent-reference-element-declarations':
     consistentReferenceElementDeclarations,
+  'no-nested-template-literals': noNestedTemplateLiterals,
   'no-cs-prefixed-event-name': noPrefixedEventName,
   'prefer-closed-shadow-root': preferClosedShadowRoot,
   'prefixed-lit-element-class-declaration': prefixedClassDeclaration,
