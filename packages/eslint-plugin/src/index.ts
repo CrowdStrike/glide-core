d3f26e92c9d67697fa7bd71c8875c20c3675e572
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2024-06-07 12:36:17 -0400

    Add lint rule around property decorator type string

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,14 +1,16 @@
 import { consistentReferenceElementDeclarations } from './rules/consistent-reference-element-declarations.js';
 import { noNestedTemplateLiterals } from './rules/no-nested-template-literals.js';
 import { noPrefixedEventName } from './rules/no-cs-prefixed-event-name.js';
+import { noRedudantPropertyStringType } from './rules/no-redundant-property-string-type.js';
 import { preferClosedShadowRoot } from './rules/prefer-closed-shadow-root.js';
 import { prefixedClassDeclaration } from './rules/prefixed-lit-element-class-declaration.js';
 
 const rules = {
   'consistent-reference-element-declarations':
     consistentReferenceElementDeclarations,
-  'no-nested-template-literals': noNestedTemplateLiterals,
   'no-cs-prefixed-event-name': noPrefixedEventName,
+  'no-nested-template-literals': noNestedTemplateLiterals,
+  'no-redundant-property-string-type': noRedudantPropertyStringType,
   'prefer-closed-shadow-root': preferClosedShadowRoot,
   'prefixed-lit-element-class-declaration': prefixedClassDeclaration,
 };
