c63e87ec63457e3036caae06b0738c71ed4e227e
Cam Skene <camskene@gmail.com>
2024-06-14 14:17:21 -0400

    use figma as source of truth for variables

---
+++ packages/components/tokens.config.js
@@ -0,0 +1,37 @@
+export default {
+  themes: [
+    {
+      name: 'dark',
+      colorScheme: 'dark',
+      tokenKey: 'dark',
+    },
+    {
+      name: 'light',
+      colorScheme: 'light',
+      tokenKey: 'light',
+    },
+    {
+      name: 'system',
+      colorScheme: 'normal',
+      tokenKey: 'primitive',
+    },
+  ],
+  excludeCollections(collection) {
+    const collectionIds = [
+      'VariableCollectionId:14:62',
+      'VariableCollectionId:44:2502',
+    ];
+
+    return !collectionIds.includes(collection.id);
+  },
+  excludeVariables(variable) {
+    const variableNames = ['color', 'component', 'data-viz'];
+
+    for (const name of variableNames) {
+      if (variable.name.includes(name)) {
+        return true;
+      }
+    }
+  },
+  outputDirectoryForStyles: 'src/styles/variables',
+};
