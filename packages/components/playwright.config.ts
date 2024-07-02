8d87ab6715791d9906622bcbd7f6f547a5cf9cb1
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2024-07-02 11:48:42 -0400

    Troubleshoot playwright images

--- packages/components/playwright.config.ts
+++ packages/components/playwright.config.ts
@@ -57,10 +57,4 @@ export default defineConfig({
     url: 'http://localhost:6006/',
     reuseExistingServer: !process.env.CI,
   },
-
-  expect: {
-    // https://github.com/microsoft/playwright/issues/20097#issuecomment-1382672908
-    // @ts-expect-error experimental feature
-    toHaveScreenshot: { _comparator: 'ssim-cie94' },
-  },
 });
