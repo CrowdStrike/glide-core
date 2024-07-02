efd20be9d3216d212e43c0f47ba6c1e904e3ac4a
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2024-07-01 20:02:00 -0400

    Add theme switcher to storybook

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -3,10 +3,27 @@
 import '../src/styles/fonts.css';
 import '../src/styles/variables.css';
 import './overrides.css';
-
 import { create } from '@storybook/theming/create';
+import { html } from 'lit';
 
 export default {
+  decorators: [
+    (story, context) => {
+      const selectedTheme = context.globals.theme || 'light';
+
+      if (selectedTheme === 'dark') {
+        document.documentElement.classList.remove('theme-light');
+        document.documentElement.classList.add('theme-dark');
+      }
+
+      if (selectedTheme === 'light') {
+        document.documentElement.classList.remove('theme-dark');
+        document.documentElement.classList.add('theme-light');
+      }
+
+      return html`${story()}`;
+    },
+  ],
   parameters: {
     docs: {
       canvas: {
@@ -17,6 +34,21 @@ export default {
         fontBase: '"Nunito", sans-serif',
         fontCode: 'monospace',
       }),
+    },
+  },
+  globalTypes: {
+    theme: {
+      description: 'Sets the global theme for components',
+      defaultValue: 'light',
+      toolbar: {
+        title: 'Theme',
+        icon: 'circlehollow',
+        items: [
+          { value: 'light', icon: 'sun', title: 'Light' },
+          { value: 'dark', icon: 'moon', title: 'Dark' },
+        ],
+        dynamicTitle: true,
+      },
     },
   },
 };
