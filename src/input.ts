0207a87dccbfc30bc83d0feebb2e3861548e41d7
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2024-08-15 09:28:20 -0400

    Remove exposing a clear-icon slot

--- src/input.ts
+++ src/input.ts
@@ -247,30 +247,28 @@ export default class GlideCoreInput extends LitElement {
                   label=${this.#localize.term('clearEntry', this.label!)}
                   @click=${this.#onClearClick}
                 >
-                  <slot name="clear-icon">
-                    <!-- X icon -->
-                    <svg
-                      aria-hidden="true"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                    >
-                      <path
-                        d="M6 6L18 18"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                      <path
-                        d="M18 6L6 18"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                  </slot>
+                  <!-- X icon -->
+                  <svg
+                    aria-hidden="true"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path
+                      d="M6 6L18 18"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M18 6L6 18"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
                 </glide-core-icon-button>
               `
             : nothing}
