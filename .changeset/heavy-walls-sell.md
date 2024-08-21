244d4c1326e0d5da8d6fd1b5ae7a4ae909b83c56
Dan Wenzel <11724146+danwenzel@users.noreply.github.com>
2024-08-21 13:13:10 -0500

    Add click method to Tree Item Menu, allowing programmatic opening of the menu

---
+++ .changeset/heavy-walls-sell.md
@@ -0,0 +1,5 @@
+---
+'@crowdstrike/glide-core': patch
+---
+
+Add click method to Tree Item Menu, allowing programmatic opening of the menu
--- src/tree.item.menu.ts
+++ src/tree.item.menu.ts
@@ -4,9 +4,10 @@ import './menu.options.js';
 import { LitElement, html } from 'lit';
 import { createRef, ref } from 'lit/directives/ref.js';
 import { customElement, property } from 'lit/decorators.js';
-import { owSlot, owSlotType } from './library/ow.js';
+import GlideCoreIconButton from './icon-button.js';
 import GlideCoreMenuButton from './menu.button.js';
 import GlideCoreMenuLink from './menu.link.js';
+import ow, { owSlot, owSlotType } from './library/ow.js';
 import styles from './tree.item.menu.styles.js';
 import type { Placement } from '@floating-ui/dom';
 import type GlideCoreMenu from './menu.js';
@@ -38,6 +39,15 @@ export default class GlideCoreTreeItemMenu extends LitElement {
   @property()
   label = '';
 
+  override click() {
+    ow(
+      this.#iconButtonElementRef.value,
+      ow.object.instanceOf(GlideCoreIconButton),
+    );
+
+    this.#iconButtonElementRef.value.click();
+  }
+
   override firstUpdated() {
     owSlot(this.#defaultSlotElementRef.value);
 
@@ -62,7 +72,11 @@ export default class GlideCoreTreeItemMenu extends LitElement {
           ></slot>
         </glide-core-menu-options>
 
-        <glide-core-icon-button slot="target" variant="tertiary">
+        <glide-core-icon-button
+          slot="target"
+          variant="tertiary"
+          ${ref(this.#iconButtonElementRef)}
+        >
           <!-- 3-dot -->
           <svg
             aria-hidden="true"
@@ -89,6 +103,8 @@ export default class GlideCoreTreeItemMenu extends LitElement {
   }
 
   #defaultSlotElementRef = createRef<HTMLSlotElement>();
+
+  #iconButtonElementRef = createRef<GlideCoreIconButton>();
 
   #menuElementRef = createRef<GlideCoreMenu>();
 
