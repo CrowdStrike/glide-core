283dd2dcdc924c3e47abc451003ddee7ffff6dad
dylankcrwd <162328827+dylankcrwd@users.noreply.github.com>
2024-08-28 12:23:42 -0400

    start toasts refactor

--- src/toasts.styles.ts
+++ src/toasts.styles.ts
@@ -1,26 +1,8 @@
 import { css } from 'lit';
+import visuallyHidden from './styles/visually-hidden.js';
 
 export default [
   css`
-    .component {
-      background-color: transparent;
-      border: none;
-      display: none;
-      flex-direction: column-reverse;
-      gap: var(--glide-core-spacing-md);
-      inline-size: 24.25rem;
-      inset-block-start: 0;
-      inset-inline-end: 0;
-      margin: 0 0 auto auto;
-      max-block-size: 100%;
-      max-inline-size: 100%;
-      overflow: hidden;
-      padding: var(--glide-core-spacing-sm);
-      position: fixed;
-
-      &:popover-open {
-        display: flex;
-      }
-    }
+    ${visuallyHidden('.component')}
   `,
 ];
