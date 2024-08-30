1ee429777e4e90a1a8d452e11565b611104327fc
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-08-30 18:17:39 -0400

    Animate checkbox's checked state (#333)

--- src/checkbox.styles.ts
+++ src/checkbox.styles.ts
@@ -56,6 +56,7 @@ when browsers support them.
       flex-shrink: 0; /* Don't shrink when the summary wraps. */
       inline-size: 100%;
       justify-content: center;
+      transition: box-shadow 200ms ease-in-out;
 
       &.error:not(.disabled) {
         border-color: var(--glide-core-status-error);
@@ -101,6 +102,24 @@ when browsers support them.
         background-color: var(--glide-core-surface-primary-disabled);
         border-color: transparent;
       }
+
+      &:checked ~ .checkbox > .checked-icon {
+        visibility: visible;
+
+        .check {
+          /*
+            Setting the animated offset to 48 is by design and aligns with the offset and array
+            properties below. 48 was chosen to ensure the animation goes from left-to-right
+            rather than right-to-left. Since our starting location is at 24 below we begin
+            at the left-most corner and need to span the entire viewbox of the SVG.
+
+            To do this, we'll multiply 24 by 2, which is the entire length of the viewbox
+            going in the opposite direction so that it'll animate from left-to-right.
+          */
+          stroke-dashoffset: 48;
+          transition: stroke-dashoffset 300ms cubic-bezier(0.32, 0, 0.67, 0);
+        }
+      }
     }
 
     .checked-icon {
@@ -108,13 +127,28 @@ when browsers support them.
 
       align-items: center;
       block-size: 100%;
-      display: none;
       inline-size: 100%;
       inset-block-start: 0;
       inset-inline-start: 0;
       justify-content: center;
       pointer-events: none;
       position: absolute;
+
+      /* We must rely on 'visibility: hidden' over 'display: none' for the animation transition to play properly. */
+      visibility: hidden;
+
+      .check {
+        /*
+          Safari doesn't support rem values for these CSS properties, otherwise we'd use calc() here.
+
+          With the path in our SVG, increasing the offset or array from 0 to 24 actually animates
+          it from right-to-left, which isn't the direction we want. To get it to go the correct
+          direction, from left-to-right, we have to begin at 24, which is the end of the SVG
+          viewbox.
+        */
+        stroke-dasharray: 24;
+        stroke-dashoffset: 24;
+      }
     }
 
     .indeterminate-icon {
@@ -131,6 +165,12 @@ when browsers support them.
       overflow: hidden;
       text-overflow: ellipsis;
       white-space: nowrap;
+    }
+
+    @media (prefers-reduced-motion: reduce) {
+      .checked-icon .check {
+        transition: none !important;
+      }
     }
   `,
 ];
--- src/icons/checked.ts
+++ src/icons/checked.ts
@@ -1,12 +1,19 @@
 import { html } from 'lit';
 
+/**
+ * The class="check" on the path below is used as a CSS selector
+ * in checkbox.styles.ts.  Please be cautious when making changes
+ * here as to not break the Checkbox selectors.
+ */
 export default html`
   <svg
+    aria-hidden="true"
     fill="none"
     viewBox="0 0 24 24"
     style="height: var(--size, 0.875rem); width: var(--size, 0.875rem);"
   >
     <path
+      class="check"
       d="M20 6L9 17L4 12"
       stroke="currentColor"
       stroke-width="2"
