2cd662484d102ac32d0b700905ebb22d3628a83c
clintcs <114178960+clintcs@users.noreply.github.com>
2024-09-23 11:05:34 -0500

    Fix Checkbox Group edge cases (#394)

--- src/checkbox-group.test.interactions.ts
+++ src/checkbox-group.test.interactions.ts
@@ -16,10 +16,11 @@ it('checks and unchecks Checkboxes when `value` is changed programmatically', as
   const component = await fixture<GlideCoreCheckboxGroup>(
     html`<glide-core-checkbox-group label="Checkbox Group">
       <glide-core-checkbox label="One" value="one"></glide-core-checkbox>
+      <glide-core-checkbox label="Two" checked></glide-core-checkbox>
 
       <glide-core-checkbox
-        label="Two"
-        value="two"
+        label="Three"
+        value="three"
         checked
       ></glide-core-checkbox>
     </glide-core-checkbox-group>`,
@@ -30,7 +31,8 @@ it('checks and unchecks Checkboxes when `value` is changed programmatically', as
   const checkboxes = component.querySelectorAll('glide-core-checkbox');
 
   expect(checkboxes[0].checked).to.be.true;
-  expect(checkboxes[1].checked).to.be.false;
+  expect(checkboxes[1].checked).to.be.true;
+  expect(checkboxes[2].checked).to.be.false;
 });
 
 it('updates `value` when the `value` of a checked Checkbox is changed programmatically', async () => {
@@ -85,4 +87,27 @@ it('updates `value` when the `value` of a checked Checkbox is emptied programmat
 
   expect(component.value).to.deep.equal(['two']);
   expect(component.getAttribute('value')).to.equal('["two"]');
+});
+
+it('enables disabled Checkboxes when `value` is set programmatically', async () => {
+  const component = await fixture<GlideCoreCheckboxGroup>(
+    html`<glide-core-checkbox-group label="Checkbox Group">
+      <glide-core-checkbox
+        label="One"
+        value="one"
+        disabled
+      ></glide-core-checkbox>
+
+      <glide-core-checkbox
+        label="Two"
+        value="two"
+        checked
+      ></glide-core-checkbox>
+    </glide-core-checkbox-group>`,
+  );
+
+  component.value = ['one', 'two'];
+
+  const checkbox = component.querySelector('glide-core-checkbox');
+  expect(checkbox?.disabled).to.be.false;
 });
