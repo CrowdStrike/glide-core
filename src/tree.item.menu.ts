42d162f112b3ac10ab0e0976b0b335e30338159d
Dan Wenzel <11724146+danwenzel@users.noreply.github.com>
2024-10-10 15:06:23 -0500

    Allow custom target icon for Tree Item Menu (#428)
    * Allow custom target icon for Tree Item Menu
    
    * Move Tree Item icon-container gaps out of :slotted

--- src/tree.item.menu.ts
+++ src/tree.item.menu.ts
@@ -3,7 +3,8 @@ import './menu.js';
 import './menu.options.js';
 import { LitElement, html } from 'lit';
 import { createRef, ref } from 'lit/directives/ref.js';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
+import { when } from 'lit/directives/when.js';
 import GlideCoreIconButton from './icon-button.js';
 import GlideCoreMenuButton from './menu.button.js';
 import GlideCoreMenuLink from './menu.link.js';
@@ -75,34 +76,50 @@ export default class GlideCoreTreeItemMenu extends LitElement {
           label=${this.label}
           ${ref(this.#iconButtonElementRef)}
         >
-          <!-- 3-dot -->
-          <svg
-            aria-hidden="true"
-            width="4"
-            height="14"
-            viewBox="0 0 4 18"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              d="M2 15C2.55228 15 3 15.4477 3 16C3 16.5523 2.55228 17 2 17C1.44772 17 1 16.5523 1 16C1 15.4477 1.44772 15 2 15Z"
-            />
-            <path
-              d="M2 8C2.55228 8 3 8.44772 3 9C3 9.55228 2.55228 10 2 10C1.44772 10 1 9.55228 1 9C1 8.44772 1.44772 8 2 8Z"
-            />
-            <path
-              d="M2 1C2.55228 1 3 1.44772 3 2C3 2.55228 2.55228 3 2 3C1.44772 3 1 2.55228 1 2C1 1.44772 1.44772 1 2 1Z"
-            />
-          </svg>
+          <slot
+            name="icon"
+            @slotchange=${this.#onIconSlotChange}
+            ${ref(this.#iconSlotElementRef)}
+          ></slot>
+
+          ${when(
+            !this.hasCustomIcon,
+            () => html`
+              <!-- 3-dot -->
+              <svg
+                aria-hidden="true"
+                width="4"
+                height="14"
+                viewBox="0 0 4 18"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  d="M2 15C2.55228 15 3 15.4477 3 16C3 16.5523 2.55228 17 2 17C1.44772 17 1 16.5523 1 16C1 15.4477 1.44772 15 2 15Z"
+                />
+                <path
+                  d="M2 8C2.55228 8 3 8.44772 3 9C3 9.55228 2.55228 10 2 10C1.44772 10 1 9.55228 1 9C1 8.44772 1.44772 8 2 8Z"
+                />
+                <path
+                  d="M2 1C2.55228 1 3 1.44772 3 2C3 2.55228 2.55228 3 2 3C1.44772 3 1 2.55228 1 2C1 1.44772 1.44772 1 2 1Z"
+                />
+              </svg>
+            `,
+          )}
         </glide-core-icon-button>
       </glide-core-menu>
     `;
   }
 
+  @state()
+  private hasCustomIcon = false;
+
   #defaultSlotElementRef = createRef<HTMLSlotElement>();
 
   #iconButtonElementRef = createRef<GlideCoreIconButton>();
+
+  #iconSlotElementRef = createRef<HTMLSlotElement>();
 
   #menuElementRef = createRef<GlideCoreMenu>();
 
@@ -113,5 +130,10 @@ export default class GlideCoreTreeItemMenu extends LitElement {
       GlideCoreMenuButton,
       GlideCoreMenuLink,
     ]);
+  }
+
+  #onIconSlotChange() {
+    const assignedNodes = this.#iconSlotElementRef.value?.assignedNodes();
+    this.hasCustomIcon = Boolean(assignedNodes && assignedNodes.length > 0);
   }
 }
