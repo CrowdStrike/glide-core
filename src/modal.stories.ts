34f364e566467ef7dd879075ab9a87a8bd28f5e6
clintcs <114178960+clintcs@users.noreply.github.com>
2025-01-22 07:02:22 -0500

    Update src/modal.stories.ts

--- src/modal.stories.ts
+++ src/modal.stories.ts
@@ -78,7 +78,6 @@ const meta: Meta = {
         ${unsafeHTML(arguments_['slot="default"'])}
 
         <glide-core-button label="Primary" slot="primary"></glide-core-button>
-        <input type="file" />
 
         <glide-core-button
           label="Secondary"
