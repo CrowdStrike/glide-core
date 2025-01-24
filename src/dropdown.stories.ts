ca48f221e140fde8ada7378422081af3c8b70730
clintcs <114178960+clintcs@users.noreply.github.com>
2025-01-24 14:02:50 -0500

    Apply suggestions from code review

--- src/dropdown.stories.ts
+++ src/dropdown.stories.ts
@@ -34,7 +34,7 @@ const meta: Meta = {
     },
   },
   args: {
-    label: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    label: 'One',
     placeholder: 'Placeholder',
     'slot="default"': '',
     'add-button-label': '',
