2c91084b107bcccfc54758118db84c77af43c1e8
clintcs <114178960+clintcs@users.noreply.github.com>
2025-01-27 14:21:55 -0500

    Add `@final` decorator (#645)

---
+++ src/library/final.test.ts
@@ -0,0 +1,41 @@
+import { LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { expect } from '@open-wc/testing';
+import sinon from 'sinon';
+import final from './final.js';
+
+@customElement('glide-core-final')
+@final
+class GlideCoreFinal extends LitElement {}
+
+@customElement('glide-core-extended')
+class GlideCoreExtended extends GlideCoreFinal {}
+
+it('throws when a class is extended', async () => {
+  const spy = sinon.spy();
+
+  try {
+    new GlideCoreExtended();
+  } catch (error) {
+    spy(error);
+  }
+
+  expect(spy.callCount).to.equal(1);
+  expect(spy.args.at(0)?.at(0) instanceof TypeError).to.be.true;
+
+  expect(spy.args.at(0)?.at(0).message).to.equal(
+    'GlideCoreFinal does not allow extension.',
+  );
+});
+
+it('does not throw when a class is not extended', () => {
+  const spy = sinon.spy();
+
+  try {
+    new GlideCoreFinal();
+  } catch (error) {
+    spy(error);
+  }
+
+  expect(spy.callCount).to.equal(0);
+});
