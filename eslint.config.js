02b05319f8f00a0dcd5f6c08e2710770d0a765ae
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2025-01-28 11:29:15 -0500

    Move test-only rules to their own block (#652)

--- eslint.config.js
+++ eslint.config.js
@@ -38,18 +38,12 @@ export default [
     rules: {
       '@crowdstrike/glide-core/consistent-reference-element-declarations':
         'error',
-      '@crowdstrike/glide-core/consistent-test-fixture-variable-declarator':
-        'error',
       '@crowdstrike/glide-core/no-glide-core-prefixed-event-name': 'error',
       '@crowdstrike/glide-core/no-nested-template-literals': 'error',
-      '@crowdstrike/glide-core/no-only-tests': 'error',
       '@crowdstrike/glide-core/no-redundant-property-attribute': 'error',
       '@crowdstrike/glide-core/no-redundant-property-string-type': 'error',
-      '@crowdstrike/glide-core/no-skip-tests': 'error',
       '@crowdstrike/glide-core/no-space-press': 'error',
-      '@crowdstrike/glide-core/no-to-have-attribute': 'error',
       '@crowdstrike/glide-core/prefer-shadow-root-mode': 'error',
-      '@crowdstrike/glide-core/prefer-to-be-true-or-false': 'error',
       '@crowdstrike/glide-core/prefixed-lit-element-class-declaration': 'error',
 
       // Enabling this rule would force us to `await` any function that returns a promise.
@@ -306,6 +300,13 @@ export default [
       // One-off components in tests can do whatever they need to with their shadow
       // roots. Though most will stick with Lit's default, which is open.
       '@crowdstrike/glide-core/prefer-shadow-root-mode': 'off',
+
+      '@crowdstrike/glide-core/consistent-test-fixture-variable-declarator':
+        'error',
+      '@crowdstrike/glide-core/no-only-tests': 'error',
+      '@crowdstrike/glide-core/no-skip-tests': 'error',
+      '@crowdstrike/glide-core/no-to-have-attribute': 'error',
+      '@crowdstrike/glide-core/prefer-to-be-true-or-false': 'error',
     },
   },
 ];
