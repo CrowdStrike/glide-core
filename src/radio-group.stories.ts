ff67e5159cbdc6377f0a17120f557884fc42cd3d
clintcs <114178960+clintcs@users.noreply.github.com>
2025-01-28 11:46:16 -0500

    Apply suggestions from code review

--- src/radio-group.stories.ts
+++ src/radio-group.stories.ts
@@ -116,7 +116,7 @@ const meta: Meta = {
     `;
   },
   args: {
-    label: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    label: 'Label',
     'slot="default"': '',
     'addEventListener(event, handler)': '',
     'checkValidity()': '',
