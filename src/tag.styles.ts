075716946e4a143adafbac235f471e8d245a86ce
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2025-03-11 08:36:53 -0400

    Audit and update Tag

--- src/tag.styles.ts
+++ src/tag.styles.ts
@@ -29,7 +29,7 @@ export default [
 
     .component {
       align-items: center;
-      background: var(--glide-core-color-static-surface-header);
+      background: var(--glide-core-color-static-surface-container-secondary);
       border-radius: var(--glide-core-rounding-base-radius-round);
       color: var(--glide-core-color-static-text-default);
       display: flex;
