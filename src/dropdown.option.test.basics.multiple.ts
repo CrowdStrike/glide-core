81898cd4c5fb1ef9620f6015b3fe17b769924f26
clintcs <114178960+clintcs@users.noreply.github.com>
2025-03-21 14:05:37 -0400

    DropdownOption disabled fixes

--- src/dropdown.option.test.basics.multiple.ts
+++ src/dropdown.option.test.basics.multiple.ts
@@ -44,3 +44,36 @@ it('is editable', async () => {
   const button = host.shadowRoot?.querySelector('[data-test="edit-button"]');
   expect(button?.checkVisibility()).to.be.true;
 });
+
+it('checks its checkbox when programmatically enabled', async () => {
+  const host = await fixture<GlideCoreDropdownOption>(
+    html`<glide-core-dropdown-option
+      label="Label"
+      private-multiple
+      disabled
+      selected
+    ></glide-core-dropdown-option>`,
+  );
+
+  host.disabled = false;
+  await host.updateComplete;
+
+  const checkbox = host.shadowRoot?.querySelector('glide-core-checkbox');
+  expect(checkbox?.checked).to.be.true;
+});
+
+it('unchecks its checkbox when programmatically disabled', async () => {
+  const host = await fixture<GlideCoreDropdownOption>(
+    html`<glide-core-dropdown-option
+      label="Label"
+      private-multiple
+      selected
+    ></glide-core-dropdown-option>`,
+  );
+
+  host.disabled = true;
+  await host.updateComplete;
+
+  const checkbox = host.shadowRoot?.querySelector('glide-core-checkbox');
+  expect(checkbox?.checked).to.be.false;
+});
