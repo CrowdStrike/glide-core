f7ede04dc25e9e67c9015f436ee3bea0d6711b38
clintcs <114178960+clintcs@users.noreply.github.com>
2025-04-04 09:33:27 -0400

    Add code comment

--- src/spinner.test.aria.ts
+++ src/spinner.test.aria.ts
@@ -1,5 +1,8 @@
 import { expect, test } from '@playwright/test';
 
+// Not sure what to name this test. It's not really testing `size="medium"`.
+// It's testing the component in its default state because there aren't
+// specific attributes that need to be tested.
 test('size="medium"', async ({ page }, test) => {
   await page.goto('?id=spinner--spinner');
   await page.locator('glide-core-spinner').waitFor();
