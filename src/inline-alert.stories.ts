b69f8c08131cb8d4d7c131ba4dea96e496a20c51
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2025-04-28 09:00:21 -0400

    InlineAlert drop removable attribute

--- src/inline-alert.stories.ts
+++ src/inline-alert.stories.ts
@@ -1,12 +1,10 @@
 import './inline-alert.js';
 import { html, nothing } from 'lit';
-import { withActions } from '@storybook/addon-actions/decorator';
 import type { Meta, StoryObj } from '@storybook/web-components';
 
 const meta: Meta = {
   title: 'Inline Alert',
   decorators: [
-    withActions,
     (story) => html`
       <script type="ignore">
         import '@crowdstrike/glide-core/inline-alert.js';
@@ -15,15 +13,8 @@ const meta: Meta = {
       ${story()}
     `,
   ],
-  parameters: {
-    actions: {
-      handles: ['remove'],
-    },
-  },
   args: {
     'slot="default"': 'Content',
-    'addEventListener(event, handler)': '',
-    removable: false,
     variant: 'informational',
     version: '',
   },
@@ -33,21 +24,6 @@ const meta: Meta = {
         type: { summary: 'Element | string' },
       },
       type: { name: 'string', required: true },
-    },
-    'addEventListener(event, handler)': {
-      control: false,
-      table: {
-        type: {
-          summary: 'method',
-          detail: '(event: "remove", handler: (event: Event) => void): void',
-        },
-      },
-    },
-    removable: {
-      defaultValue: { summary: 'false' },
-      table: {
-        type: { summary: 'boolean' },
-      },
     },
     variant: {
       control: { type: 'radio' },
