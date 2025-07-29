36b59849e120a564176101346599deee19785d4d
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2025-07-29 12:39:07 -0400

    Rename TabGroup to Tabs

--- src/tabs.ts
+++ src/tabs.ts
@@ -10,14 +10,14 @@ import Tab from './tab.js';
 import TabPanel from './tab.panel.js';
 import chevronIcon from './icons/chevron.js';
 import onResize from './library/on-resize.js';
-import styles from './tab.group.styles.js';
+import styles from './tabs.styles.js';
 import assertSlot from './library/assert-slot.js';
 import shadowRootMode from './library/shadow-root-mode.js';
 import final from './library/final.js';
 
 declare global {
   interface HTMLElementTagNameMap {
-    'glide-core-tab-group': TabGroup;
+    'glide-core-tabs': Tabs;
   }
 }
 
@@ -33,9 +33,9 @@ declare global {
  * @cssprop [--tabs-padding-inline-end=0rem]
  * @cssprop [--tabs-padding-inline-start=0rem]
  */
-@customElement('glide-core-tab-group')
+@customElement('glide-core-tabs')
 @final
-export default class TabGroup extends LitElement {
+export default class Tabs extends LitElement {
   static override shadowRootOptions: ShadowRootInit = {
     ...LitElement.shadowRootOptions,
     mode: shadowRootMode,
