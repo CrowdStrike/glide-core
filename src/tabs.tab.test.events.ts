37eb54964377caa125d7974a12edba58d3c4f2fd
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2025-08-11 12:34:02 -0400

    Remove extra logic in #onTabListResize()

--- src/tabs.tab.test.events.ts
+++ src/tabs.tab.test.events.ts
@@ -15,3 +15,36 @@ it('dispatches a "private-selected" event when selected', async () => {
   expect(event instanceof Event).to.be.true;
   expect(event.bubbles).to.be.true;
 });
+
+it('dispatches a "private-label-change" event when the `label` is changed', async () => {
+  const host = await fixture<TabsTab>(
+    html`<glide-core-tabs-tab label="Label" panel="1"></glide-core-tabs-tab>`,
+  );
+
+  setTimeout(() => {
+    host.label = 'Two';
+  });
+
+  const event = await oneEvent(host, 'private-label-change');
+
+  expect(event instanceof Event).to.be.true;
+  expect(event.bubbles).to.be.true;
+});
+
+it('dispatches a "private-icon-slotchange" event when the icon slot is changed', async () => {
+  const host = await fixture<TabsTab>(
+    html`<glide-core-tabs-tab label="Label" panel="1"> </glide-core-tabs-tab>`,
+  );
+
+  setTimeout(() => {
+    const div = document.createElement('div');
+    div.setAttribute('slot', 'icon');
+    div.textContent = 'two';
+    host.append(div);
+  });
+
+  const event = await oneEvent(host, 'private-icon-slotchange');
+
+  expect(event instanceof Event).to.be.true;
+  expect(event.bubbles).to.be.true;
+});
