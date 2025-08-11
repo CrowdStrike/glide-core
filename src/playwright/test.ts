ae1eb1c031d0e4be8379af89e43ac9d9c8340367
clintcs <114178960+clintcs@users.noreply.github.com>
2025-08-11 11:22:12 -0400

    Remove `toBeFocused()` matcher

--- src/playwright/test.ts
+++ src/playwright/test.ts
@@ -11,19 +11,17 @@ import mount from './fixtures/mount.js';
 import removeAttribute from './fixtures/remove-attribute.js';
 import setAttribute from './fixtures/set-attribute.js';
 import setProperty from './fixtures/set-property.js';
-import toBeFocused from './matchers/to-be-focused.js';
 import toBeRegistered from './matchers/to-be-registered.js';
 import toDispatchEvents from './matchers/to-dispatch-events.js';
 import toHaveFormData from './matchers/to-have-form-data.js';
 
 export const expect = mergeExpects(
-  toBeFocused,
   toBeRegistered,
   toDispatchEvents,
   toHaveFormData,
 ) as Expect<{
-  toBeFocused: (locator: Locator) => Promise<void>;
   toBeRegistered: (locator: Locator, name: string) => Promise<void>;
+
   toDispatchEvents: (
     locator: Locator,
     action: () => Promise<unknown>,
@@ -35,6 +33,7 @@ export const expect = mergeExpects(
       type: string;
     }[],
   ) => Promise<void>;
+  
   toHaveFormData: (
     locator: Locator,
     name: string,
