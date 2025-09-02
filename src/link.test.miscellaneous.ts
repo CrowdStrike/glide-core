743d6fcb9724703119a22f1a1eb9ddbfff0affa7
ynotdraw <8069555+ynotdraw@users.noreply.github.com>
2025-09-02 15:58:08 -0400

    Migrate Link tests

---
+++ src/link.test.miscellaneous.ts
@@ -0,0 +1,36 @@
+import { html } from 'lit';
+import { expect, test } from './playwright/test.js';
+
+test('defines itself', { tag: '@miscellaneous' }, async ({ mount, page }) => {
+  await mount(
+    () => html`<glide-core-link label="Label" href="/"></glide-core-link>`,
+  );
+
+  const host = page.locator('glide-core-link');
+
+  await expect(host).toBeDefined('glide-core-link');
+});
+
+test(
+  'throws when `label` is undefined',
+  { tag: '@miscellaneous' },
+  async ({ mount }) => {
+    await expect(
+      mount(() => html`<glide-core-link></glide-core-link>`),
+    ).rejects.toThrow();
+  },
+);
+
+test(
+  'cannot be extended',
+  { tag: '@miscellaneous' },
+  async ({ mount, page }) => {
+    await mount(
+      () => html`<glide-core-link label="Label" href="/"></glide-core-link>`,
+    );
+
+    const host = page.locator('glide-core-link');
+
+    await expect(host).not.toBeExtensible();
+  },
+);
---
+++ src/link.test.mouse.ts
@@ -0,0 +1,104 @@
+import { html } from 'lit';
+import { expect, test } from './playwright/test.js';
+
+test(
+  'dispatches a "click" event on click',
+  { tag: '@mouse' },
+  async ({ mount, page }) => {
+    await mount(
+      () => html`<glide-core-link label="Label" href="/"></glide-core-link>`,
+    );
+
+    const host = page.locator('glide-core-link');
+
+    await page.evaluate(() => {
+      document.addEventListener('click', (event) => {
+        event.preventDefault();
+      });
+    });
+
+    await expect(host).toDispatchEvents(
+      () => host.click(),
+      [
+        {
+          bubbles: true,
+          composed: true,
+          type: 'click',
+        },
+      ],
+    );
+  },
+);
+
+test(
+  'does not dispatch a "click" event on click when disabled',
+  { tag: '@mouse' },
+  async ({ mount, page }) => {
+    await mount(
+      () =>
+        html`<glide-core-link
+          label="Label"
+          href="/"
+          disabled
+        ></glide-core-link>`,
+    );
+
+    const host = page.locator('glide-core-link');
+
+    await expect(host).not.toDispatchEvents(
+      () => host.click(),
+      [{ type: 'click' }],
+    );
+  },
+);
+
+test(
+  'dispatches a "click" event on `click()`',
+  { tag: '@mouse' },
+  async ({ callMethod, mount, page }) => {
+    await mount(
+      () => html`<glide-core-link label="Label" href="/"></glide-core-link>`,
+    );
+
+    const host = page.locator('glide-core-link');
+
+    await page.evaluate(() => {
+      document.addEventListener('click', (event) => {
+        event.preventDefault();
+      });
+    });
+
+    await expect(host).toDispatchEvents(
+      () => callMethod(host, 'click'),
+      [
+        {
+          bubbles: true,
+          composed: true,
+          type: 'click',
+        },
+      ],
+    );
+  },
+);
+
+test(
+  'does not dispatch a "click" event on `click()` when disabled',
+  { tag: '@mouse' },
+  async ({ callMethod, mount, page }) => {
+    await mount(
+      () =>
+        html`<glide-core-link
+          label="Label"
+          href="/"
+          disabled
+        ></glide-core-link>`,
+    );
+
+    const host = page.locator('glide-core-link');
+
+    await expect(host).not.toDispatchEvents(
+      () => callMethod(host, 'click'),
+      [{ type: 'click' }],
+    );
+  },
+);
