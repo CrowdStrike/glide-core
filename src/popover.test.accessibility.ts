c2a0a9d87c21a2fa1d91b5250cfaffa97f77afd0
clintcs <114178960+clintcs@users.noreply.github.com>
2025-09-15 13:28:48 -0400

    Migrate Popover's tests

--- src/popover.test.accessibility.ts
+++ src/popover.test.accessibility.ts
@@ -1,5 +1,138 @@
+import { html } from 'lit';
 import { expect, test } from './playwright/test.js';
 import type Popover from './popover.js';
+
+test('is accessible', { tag: '@accessibility' }, async ({ mount, page }) => {
+  await mount(
+    () =>
+      html`<glide-core-popover>
+        Popover
+        <button slot="target">Target</button>
+      </glide-core-popover>`,
+  );
+
+  await expect(page).toBeAccessible('glide-core-popover');
+});
+
+test(
+  'sets `aria-expanded` on its target when open initially',
+  { tag: '@accessibility' },
+  async ({ mount, page }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open>
+          Popover
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const button = page.getByRole('button');
+
+    await expect(button).toHaveAttribute('aria-expanded', 'true');
+  },
+);
+
+test(
+  'sets `aria-expanded` on its target when opened programmatically',
+  { tag: '@accessibility' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover>
+          Popover
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const button = page.getByRole('button');
+
+    await setProperty(host, 'open', true);
+
+    await expect(button).toHaveAttribute('aria-expanded', 'true');
+  },
+);
+
+test(
+  'sets `aria-expanded` on its target when closed initially',
+  { tag: '@accessibility' },
+  async ({ mount, page }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover>
+          Popover
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const button = page.getByRole('button');
+
+    await expect(button).toHaveAttribute('aria-expanded', 'false');
+  },
+);
+
+test(
+  'sets `aria-expanded` on its target when closed programmatically',
+  { tag: '@accessibility' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open>
+          Popover
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const button = page.getByRole('button');
+
+    await setProperty(host, 'open', false);
+
+    await expect(button).toHaveAttribute('aria-expanded', 'false');
+  },
+);
+
+test(
+  'sets `aria-expanded` on its target when enabled programmatically',
+  { tag: '@accessibility' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover disabled open>
+          Popover
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const button = page.getByRole('button');
+
+    await setProperty(host, 'disabled', false);
+
+    await expect(button).toHaveAttribute('aria-expanded', 'true');
+  },
+);
+
+test(
+  'sets `aria-expanded` on its target when disabled programmatically',
+  { tag: '@accessibility' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open>
+          Popover
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const button = page.getByRole('button');
+
+    await setProperty(host, 'disabled', true);
+
+    await expect(button).toHaveAttribute('aria-expanded', 'false');
+  },
+);
 
 test('disabled=${true}', { tag: '@accessibility' }, async ({ page }) => {
   await page.goto('?id=popover--popover');
---
+++ src/popover.test.miscellaneous.ts
@@ -0,0 +1,301 @@
+import { html } from 'lit';
+import { expect, test } from './playwright/test.js';
+
+test('defines itself', { tag: '@miscellaneous' }, async ({ mount, page }) => {
+  await mount(
+    () =>
+      html`<glide-core-popover>
+        Popover
+        <button slot="target">Target</button>
+      </glide-core-popover>`,
+  );
+
+  const host = page.locator('glide-core-popover');
+
+  await expect(host).toBeInTheCustomElementRegistry('glide-core-popover');
+});
+
+test(
+  'can be open initially',
+  { tag: '@miscellaneous' },
+  async ({ mount, page }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const content = page.getByTestId('content');
+
+    await expect(content).toBeVisible();
+  },
+);
+
+test(
+  'can be opened programmatically',
+  { tag: '@miscellaneous' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await expect(host).toDispatchEvents(
+      () => setProperty(host, 'open', true),
+      [
+        {
+          type: 'toggle',
+          bubbles: true,
+          cancelable: false,
+          composed: true,
+        },
+      ],
+    );
+
+    await expect(host).toHaveAttribute('open');
+    await expect(content).toBeVisible();
+  },
+);
+
+test(
+  'can be closed programmatically',
+  { tag: '@miscellaneous' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await expect(host).toDispatchEvents(
+      () => setProperty(host, 'open', false),
+      [
+        {
+          type: 'toggle',
+          bubbles: true,
+          cancelable: false,
+          composed: true,
+        },
+      ],
+    );
+
+    await expect(host).not.toHaveAttribute('open');
+    await expect(content).toBeHidden();
+  },
+);
+
+test(
+  'remains open when opened programmatically and already open',
+  { tag: '@miscellaneous' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await expect(host).not.toDispatchEvents(
+      () => setProperty(host, 'open', true),
+      [
+        {
+          type: 'toggle',
+          bubbles: true,
+          cancelable: false,
+          composed: true,
+        },
+      ],
+    );
+
+    await expect(host).toHaveAttribute('open');
+    await expect(content).toBeVisible();
+  },
+);
+
+test(
+  'remains closed when opened programmatically and already closed',
+  { tag: '@miscellaneous' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await expect(host).not.toDispatchEvents(
+      () => setProperty(host, 'open', false),
+      [
+        {
+          type: 'toggle',
+          bubbles: true,
+          cancelable: false,
+          composed: true,
+        },
+      ],
+    );
+
+    await expect(host).not.toHaveAttribute('open');
+    await expect(content).toBeHidden();
+  },
+);
+
+test(
+  'is not opened initially when disabled',
+  { tag: '@miscellaneous' },
+  async ({ mount, page }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover disabled open>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await expect(host).toHaveAttribute('open');
+    await expect(content).toBeHidden();
+  },
+);
+
+test(
+  'cannot be opened programmatically when disabled',
+  { tag: '@miscellaneous' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover disabled>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await expect(host).not.toDispatchEvents(
+      () => setProperty(host, 'open', true),
+      [
+        {
+          type: 'toggle',
+          bubbles: true,
+          cancelable: false,
+          composed: true,
+        },
+      ],
+    );
+
+    await expect(host).toHaveAttribute('open');
+    await expect(content).toBeHidden();
+  },
+);
+
+test(
+  'opens when enabled programmatically',
+  { tag: '@miscellaneous' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open disabled>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await setProperty(host, 'disabled', false);
+
+    await expect(host).toHaveAttribute('open');
+    await expect(content).toBeVisible();
+  },
+);
+
+test(
+  'closes when disabled programmatically',
+  { tag: '@miscellaneous' },
+  async ({ mount, page, setProperty }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover open>
+          <div data-test="content">Content</div>
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+    const content = page.getByTestId('content');
+
+    await setProperty(host, 'disabled', true);
+
+    await expect(host).toHaveAttribute('open');
+    await expect(content).toBeHidden();
+  },
+);
+
+test(
+  'cannot be extended',
+  { tag: '@miscellaneous' },
+  async ({ mount, page }) => {
+    await mount(
+      () =>
+        html`<glide-core-popover>
+          Content
+          <button slot="target">Target</button>
+        </glide-core-popover>`,
+    );
+
+    const host = page.locator('glide-core-popover');
+
+    await expect(host).not.toBeExtensible();
+  },
+);
+
+test(
+  'throws when it has no default slot',
+  { tag: '@miscellaneous' },
+  async ({ mount }) => {
+    await expect(
+      mount(
+        () =>
+          html`<glide-core-popover
+            ><button slot="target">Target</button></glide-core-popover
+          >`,
+      ),
+    ).rejects.toThrow();
+  },
+);
+
+test(
+  'throws when it has no "target" slot',
+  { tag: '@miscellaneous' },
+  async ({ mount }) => {
+    await expect(
+      mount(() => html`<glide-core-popover> Popover </glide-core-popover>`),
+    ).rejects.toThrow();
+  },
+);
